6c01665f97f61f9cf26e8d81ba7ffcf4cfde03ed
changhoi <0417kch@naver.com>
2019-08-10 20:21:24 +0900

    feat(db): setup postgresql

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+require("dotenv").config();
+
 import { Options } from "graphql-yoga";
 import { createConnection } from "typeorm";
 
 import app from "./app";
 import connectionOptions from "./ormConfig";
+
+console.log(process.env.PORT, process.env.DB_USERNAME);
 
 const PORT: number | string = process.env.PORT || 4000;
 const PLAYGROUND: string = "/playground";
